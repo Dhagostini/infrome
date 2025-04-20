005da6bf76e2afbd05ade012a7fda8d9cf56abde
Dhagostini <dhagostini@hotmail.com>
2025-04-20 20:10:14 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,8 @@ from bs4 import BeautifulSoup
 # Desabilita warnings de SSL
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# URLs base\ HEADERS = {
+# URLs base
+HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                   'AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/100.0.4896.127 Safari/537.36'
